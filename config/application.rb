01bc211c018bf583e4b6bd16c7416aa1903c425f
mikurikuri1919    <mikuriyakenta003@gmail.com>
2023-03-31 11:32:36 +0900

    Complete chapter6

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,14 @@ module Taskleaf
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # タイムゾーンを日本語にする
+    config.time_zone = 'Asia/Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    
+
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
